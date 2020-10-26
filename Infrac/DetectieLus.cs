f113e3e65f6ca4cccc73ac256a6868c9f4b65977
ben ferdinandus <b.ferdinandus@4dotnettraining.onmicrosoft.com>
2020-10-26 15:37:34 +0100

    Add project files.

---
+++ Infrac/DetectieLus.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Infrac
+{
+    public class DetectieLus
+    {
+        public void Trigger()
+        {
+            Console.WriteLine("Hmmm. We zien iets...");
+            // TODO: Actieveer hier alles wat aan de detectielus hangt
+
+        }
+    }
+}
---
+++ Oprijlaan/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Heras;
+using Infrac;
+using Osram;
+using DoomsdayPreppers;
+
+namespace Oprijlaan
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Hek hek = new Hek();
+            Lamp lamp = new Lamp();
+            DetectieLus lus = new DetectieLus();
+            Valkuil kuil = new Valkuil();
+
+            lus.Trigger();
+
+        }
+    }
+}
